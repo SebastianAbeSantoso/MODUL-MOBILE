856f3fdb2282f3ab79fe3fe134d791d840fadd9f
Sebastian Abe Santoso <2410817210002@mhs.ulm.ac.id>
2026-04-18 12:19:36 +0800

    compose finally done, and now starting xml

--- MODUL 2/Compose/app/src/main/java/com/example/modul2compose/MainActivity.kt
+++ MODUL 2/Compose/app/src/main/java/com/example/modul2compose/MainActivity.kt
@@ -1,61 +1,42 @@
 package com.example.modul2compose
 
-import android.R.attr.enabled
-import android.R.attr.icon
-import android.R.attr.type
-import android.R.id.icon
 import android.annotation.SuppressLint
-import android.graphics.Paint
 import android.os.Bundle
-import android.widget.NumberPicker
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AttachMoney
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MenuAnchorType
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.modul2compose.ui.theme.Modul2ComposeTheme
-import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -131,8 +112,7 @@ fun TipContent(modifier: Modifier = Modifier) {
             onOptionSelected = { newPercentage -> selectedOption = newPercentage }
         )
 
-        val percent = selectedOption.replace("%", "")
-        var tipAmount = (amountDouble?.times(percent.toDouble())?.div(100)) ?: 0.00
+
 
         Row(
             modifier = Modifier.fillMaxWidth(0.67f)
@@ -148,7 +128,10 @@ fun TipContent(modifier: Modifier = Modifier) {
             )
         }
 
+        val percent = selectedOption.replace("%", "")
+        var tipAmount = (amountDouble?.times(percent.toDouble())?.div(100)) ?: 0.00
         if (checked) tipAmount = kotlin.math.ceil(tipAmount)
+
         val formattedTip = String.format("%.2f", tipAmount)
         Text(text = "Tip Amount: $$formattedTip", fontSize = 33.sp, fontWeight = FontWeight.Bold)
     }
@@ -195,15 +178,5 @@ fun TipPercentageDropdown(currentSelection: String, onOptionSelected: (String) -
                 )
             }
         }
-    }
-}
-
-
-
-@Preview
-@Composable
-fun MyAppPreview() {
-    Modul2ComposeTheme {
-        MyApp(Modifier, { TipContent(Modifier) })
     }
 }
