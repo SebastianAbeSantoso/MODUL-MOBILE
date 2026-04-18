ca694cf0f6f94ccc0c908acee509a18675c825ef
Sebastian Abe Santoso <2410817210002@mhs.ulm.ac.id>
2026-04-18 12:12:17 +0800

    compose done

--- MODUL 2/Compose/app/src/main/java/com/example/modul2compose/MainActivity.kt
+++ MODUL 2/Compose/app/src/main/java/com/example/modul2compose/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.modul2compose
 
+import android.R.attr.enabled
 import android.R.attr.icon
+import android.R.attr.type
 import android.R.id.icon
+import android.annotation.SuppressLint
 import android.graphics.Paint
 import android.os.Bundle
 import android.widget.NumberPicker
@@ -13,6 +16,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -28,8 +32,10 @@ import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -42,12 +48,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.modul2compose.ui.theme.Modul2ComposeTheme
+import kotlin.math.roundToInt
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -84,75 +93,112 @@ fun TipScreen(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("DefaultLocale")
 @Composable
-fun TipContent(
-    modifier: Modifier = Modifier,
-) {
+fun TipContent(modifier: Modifier = Modifier) {
     Column (
         modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ){
         var amount by remember { mutableStateOf("") }
-        var expanded by remember {mutableStateOf(false)}
-        var selectedOption by remember { mutableStateOf("") }
-            Text(modifier = Modifier
+        var checked by remember { mutableStateOf(false) }
+        val amountDouble = amount.toDoubleOrNull()
+        var selectedOption by remember { mutableStateOf("15%") }
+
+        Text(
+            modifier = Modifier
                 .fillMaxWidth(0.67f)
                 .padding(top = 20.dp, bottom = 20.dp),
-                textAlign = TextAlign.Start,
-                text = "Calculate Tip")
-        val amountDouble = amount.toDoubleOrNull() ?: 0.00
-
-        TextField (
-            value = amount,
-            onValueChange = {amount = it},
-            leadingIcon = {
-                Icon (
-                    imageVector = Icons.Default.AttachMoney,
-                    contentDescription = null
-                ) },
-            label = {Text("Bill Amount")}
+            textAlign = TextAlign.Start,
+            text = "Calculate Tip"
         )
 
-        ExposedDropdownMenuBox(
-            expanded = expanded,
-            onExpandedChange = {expanded = !expanded},
-            modifier.padding(top = 20.dp, bottom = 20.dp)
-        ) {
-
-            TextField(
-                label = {Text("Tip Percentage")},
-                leadingIcon = {Text("%")},
-                value = selectedOption,
-                onValueChange = {},
-                readOnly = true,
-                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)}
-            )
-
-            ExposedDropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false }
-            ) {
-                DropdownMenuItem(
-                    text = {Text("Option 1")},
-                    onClick = {}
+        TextField(
+            value = amount,
+            onValueChange = { amount = it },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.AttachMoney,
+                    contentDescription = null
                 )
+            },
+            label = { Text("Bill Amount") }
+        )
 
+        TipPercentageDropdown(
+            currentSelection = selectedOption,
+            onOptionSelected = { newPercentage -> selectedOption = newPercentage }
+        )
+
+        val percent = selectedOption.replace("%", "")
+        var tipAmount = (amountDouble?.times(percent.toDouble())?.div(100)) ?: 0.00
+
+        Row(
+            modifier = Modifier.fillMaxWidth(0.67f)
+                .padding(bottom = 20.dp, top = 10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "Round up tip?")
+
+            Switch(
+                checked = checked,
+                onCheckedChange = { checked = it }
+            )
+        }
+
+        if (checked) tipAmount = kotlin.math.ceil(tipAmount)
+        val formattedTip = String.format("%.2f", tipAmount)
+        Text(text = "Tip Amount: $$formattedTip", fontSize = 33.sp, fontWeight = FontWeight.Bold)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TipPercentageDropdown(currentSelection: String, onOptionSelected: (String) -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+    val tipPercentage = listOf("15%", "18%", "20%")
+
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+        modifier = Modifier.padding(top = 20.dp, bottom = 20.dp)
+    ) {
+        TextField(
+            modifier = Modifier.menuAnchor(
+                type = MenuAnchorType.PrimaryNotEditable,
+                enabled = true
+            ),
+
+            label = { Text("Tip Percentage") },
+            leadingIcon = { Text("%") },
+            value = currentSelection,
+            onValueChange = {},
+            readOnly = true,
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) }
+        )
+
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            tipPercentage.forEach { option ->
                 DropdownMenuItem(
-                    text = {Text("Option 2")},
-                    onClick = {}
+                    text = { Text(text = option) },
+                    onClick = {
+                        onOptionSelected(option)
+                        expanded = false
+                    },
+                    contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
                 )
             }
         }
-
-        /*RadioButton(){
-
-        }*/
-
-        Text(text = "Tip Amount: $$amountDouble", fontSize = 30.sp, fontWeight = FontWeight.Bold)
     }
 }
+
+
 
 @Preview
 @Composable
